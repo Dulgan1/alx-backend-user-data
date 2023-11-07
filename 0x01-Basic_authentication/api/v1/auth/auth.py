0e5f88f497f0c83fd2dc95904a09d9d47b571a24
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-11-08 00:12:26 +0100

    methods doc

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -2,17 +2,24 @@
 """ Auth module for tge API """
 from flask import request
 
+
 class Auth:
     """ Defines Authentication class Auth """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """"""
+        """
+        Checks path in excluded_path
+        """
         return False
 
     def authorization_header(self, request=None) -> str:
-        """"""
+        """
+        Chexks authorization header
+        """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """"""
+        """
+        Current user?
+        """
         return None
