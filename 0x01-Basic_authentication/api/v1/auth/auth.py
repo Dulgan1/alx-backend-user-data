19c5de9dcfd29756fe51a6fd8290bfb53ea2c11f
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-11-08 00:38:56 +0100

    Checks

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -15,10 +15,11 @@ class Auth:
             return True
 
         for p in excluded_paths:
-            if p.endswith('/') and path.startswith([:-1]) or \
+            if (p.endswith('/') and path.startswith([:-1])) or \
                     p in {path, path + '/'}:
                 return False
-        return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
