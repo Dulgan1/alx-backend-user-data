8c403ddaf9ae4d2d2a4b31e01d7bbcb394668a09
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-11-09 00:33:01 +0100

    Pycodestyle

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,8 +6,8 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Implements a basic auth class"""
 
-    def extract_base64_authorization_header(self, \
-            authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str) \
+            -> str:
         """Method extracts authorization header contents"""
         if authorization_header and authorization_header.startswith('Basic ') \
                 and isinstance(authorization_header, str):
