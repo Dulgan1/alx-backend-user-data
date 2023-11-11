faef845edd7d25c76e2deadc2eec8de73c728460
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-11-11 05:46:37 +0100

    method for task 10

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic Auth module"""
 from api.v1.auth.auth import Auth
 import base64
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -38,3 +39,19 @@ class BasicAuth(Auth):
         email, password = decoded_base64_authorization_header.split(':')
 
         return email, password
+
+    def user_object_from_credentials(
+            self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """Mwthod searchs for usee with email and password"""
+        if (user_email is None or not isinstance(user_email, str)) or \
+                (user_pwd is None or not isinstance(user_pwd, str))
+            return None
+
+        try:
+            users = User.search({'email': user_email})
+        except Exception:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
