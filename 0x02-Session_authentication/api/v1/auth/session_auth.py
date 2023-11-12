0b248fb91a4ee193ba60e975017224042537cabe
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-11-12 01:34:15 +0100

    Session auth initials

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module defines Session auth class"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """Implememt the session auth class"""
+    pass
