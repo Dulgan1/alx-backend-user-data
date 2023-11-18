b46df9aaba6af02b9d803cb3c587734bcc6f8e9d
Dulgan Danjuma <dulgandanjuma1620@gmail.com>
2023-11-19 00:44:31 +0100

    Created RedactingFormatter class

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -11,3 +11,22 @@ def filter_datum(fields: List[str], redaction: str,
         message = re.sub(field + '=.*?' + separator,
                          field + '=' + redaction + separator, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: Lost[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """Formats log with filter datum"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RadactingFormatter, self).format(record),
+                            self.SEPARATOR)
